61e14408994068f143556abfdbb4519a63fb2455
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-12-11 14:35:42 +0100

    Header - Navbar top completed (burger menu to be done)

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('src/app.js', 'public/').sass('src/app.scss', 'public/');
+mix.options({ processCssUrls : false });
 
 // Full API
 // mix.js(src, output);
